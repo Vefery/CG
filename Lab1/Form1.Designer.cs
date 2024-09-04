07ac5a1e947e29409153bcdc29aa0d0bba5bc803
Vefery <easygamer751@gmail.com>
2024-09-04 23:24:40 +0700

    basic triangles draw

---
+++ Lab1/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.openGLControl1 = new SharpGL.OpenGLControl();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.drawButton = new System.Windows.Forms.Button();
+            this.dragButton = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.openGLControl1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openGLControl1
+            // 
+            this.openGLControl1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.openGLControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.openGLControl1.DrawFPS = true;
+            this.openGLControl1.FrameRate = 60;
+            this.openGLControl1.Location = new System.Drawing.Point(100, 0);
+            this.openGLControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.openGLControl1.Name = "openGLControl1";
+            this.openGLControl1.OpenGLVersion = SharpGL.Version.OpenGLVersion.OpenGL2_1;
+            this.openGLControl1.RenderContextType = SharpGL.RenderContextType.DIBSection;
+            this.openGLControl1.RenderTrigger = SharpGL.RenderTrigger.TimerBased;
+            this.openGLControl1.Size = new System.Drawing.Size(700, 450);
+            this.openGLControl1.TabIndex = 0;
+            this.openGLControl1.OpenGLDraw += new SharpGL.RenderEventHandler(this.openGLControl1_OpenGLDraw);
+            this.openGLControl1.Resized += new System.EventHandler(this.openGLControl1_Resized);
+            this.openGLControl1.Load += new System.EventHandler(this.openGLControl1_Load);
+            this.openGLControl1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.openGLControl1_KeyDown);
+            this.openGLControl1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.openGLControl1_MouseDown);
+            this.openGLControl1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.openGLControl1_MouseMove);
+            this.openGLControl1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.openGLControl1_MouseUp);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.openGLControl1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(100, 0, 0, 0);
+            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.TabIndex = 2;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.drawButton);
+            this.flowLayoutPanel1.Controls.Add(this.dragButton);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(2);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(100, 450);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // drawButton
+            // 
+            this.drawButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.drawButton.Location = new System.Drawing.Point(3, 3);
+            this.drawButton.Name = "drawButton";
+            this.drawButton.Size = new System.Drawing.Size(95, 23);
+            this.drawButton.TabIndex = 0;
+            this.drawButton.Text = "Draw";
+            this.drawButton.UseVisualStyleBackColor = true;
+            this.drawButton.Click += new System.EventHandler(this.drawButton_Click);
+            // 
+            // dragButton
+            // 
+            this.dragButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dragButton.Location = new System.Drawing.Point(3, 32);
+            this.dragButton.Name = "dragButton";
+            this.dragButton.Size = new System.Drawing.Size(95, 23);
+            this.dragButton.TabIndex = 1;
+            this.dragButton.Text = "Move";
+            this.dragButton.UseVisualStyleBackColor = true;
+            this.dragButton.Click += new System.EventHandler(this.dragButton_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.openGLControl1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private SharpGL.OpenGLControl openGLControl1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button drawButton;
+        private System.Windows.Forms.Button dragButton;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
---
+++ Lab1/Form1.cs
@@ -0,0 +1,205 @@
+﻿using SharpGL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    public partial class Form1 : Form
+    {
+        enum Mode
+        {
+            Draw,
+            Drag
+        }
+        struct Point
+        {
+            public short x, y;
+
+            public static Point operator +(Point a, Point b)
+            {
+                Point t;
+                t.x = (short)(a.x + b.x);
+                t.y = (short)(a.y + b.y);
+                return t;
+            }
+        }
+        struct Triangle
+        {
+            public Point point1, point2, point3;
+            public Color color;
+        }
+
+        OpenGL gl;
+        Mode mode = Mode.Draw;
+        List<Triangle> tris = new List<Triangle>();
+        Point[] pointsBuffer = new Point[3];
+        byte pointsInBuffer = 0;
+        int selectedTriangle = -1;
+        Point prevMouseLocation;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openGLControl1_OpenGLDraw(object sender, RenderEventArgs args)
+        {
+            gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);	// Clear The Screen And The Depth Buffer
+            gl.LoadIdentity();
+
+            foreach (Triangle tri in tris)
+            {
+                gl.Color(tri.color.R, tri.color.G, tri.color.B);
+                gl.Begin(OpenGL.GL_TRIANGLES);
+                gl.Vertex(tri.point1.x, tri.point1.y);
+                gl.Vertex(tri.point2.x, tri.point2.y);
+                gl.Vertex(tri.point3.x, tri.point3.y);
+                gl.End();
+
+            }
+            gl.Finish();
+        }
+
+        private void openGLControl1_Load(object sender, EventArgs e)
+        {
+            gl = openGLControl1.OpenGL;
+
+            contextMenuStrip1.Items.Add("Change Color");
+        }
+
+        private void openGLControl1_MouseDown(object sender, MouseEventArgs e)
+        {
+            // Если нажата ЛКМ
+            if (e.Button == MouseButtons.Left)
+            {
+                if (mode == Mode.Draw)
+                {
+                    // Создаем точку с координатами клика и добавляем в буфер
+                    Point clickPoint;
+                    clickPoint.x = (short)e.Location.X;
+                    clickPoint.y = (short)e.Location.Y;
+                    pointsBuffer[pointsInBuffer] = clickPoint;
+                    pointsInBuffer++;
+
+                    // Если буфер полный создаем треугольник и добавляем его
+                    if (pointsInBuffer > 2)
+                    {
+                        pointsInBuffer = 0;
+                        Triangle newTri;
+                        newTri.point1 = pointsBuffer[0];
+                        newTri.point2 = pointsBuffer[1];
+                        newTri.point3 = pointsBuffer[2];
+                        newTri.color = Color.Red;
+
+                        tris.Add(newTri);
+                    }
+                } 
+                else if (mode == Mode.Drag)
+                {
+                    Point p;
+                    p.x = (short)e.X;
+                    p.y = (short)e.Y;
+ 
+                    for (int i = 0; i < tris.Count && selectedTriangle == -1; i++)
+                    {
+                        if (isInsideTriangle(tris[i], p))
+                        {
+                            selectedTriangle = i;
+                            prevMouseLocation.x = (short)e.X;
+                            prevMouseLocation.y = (short)e.Y;
+                        }
+                    }
+                }
+            } else if (e.Button == MouseButtons.Right)
+            {
+                Point p;
+                p.x = (short)e.X;
+                p.y = (short)e.Y;
+
+                for (int i = 0; i < tris.Count && selectedTriangle == -1; i++)
+                {
+                    if (isInsideTriangle(tris[i], p))
+                    {
+                        selectedTriangle = i;
+                        prevMouseLocation.x = (short)e.X;
+                        prevMouseLocation.y = (short)e.Y;
+
+                        contextMenuStrip1.Show(openGLControl1, e.Location);
+                    }
+                }
+            }
+        }
+
+        private bool isInsideTriangle(Triangle tri, Point point)
+        {
+            double det = ((tri.point2.y - tri.point3.y) * (tri.point1.x - tri.point3.x) + (tri.point3.x - tri.point2.x) * (tri.point1.y - tri.point3.y));
+            double a = ((tri.point2.y - tri.point3.y) * (point.x - tri.point3.x) + (tri.point3.x - tri.point2.x) * (point.y - tri.point3.y)) / det;
+            double b = ((tri.point3.y - tri.point1.y) * (point.x - tri.point3.x) + (tri.point1.x - tri.point3.x) * (point.y - tri.point3.y)) / det;
+            double c = 1 - a - b;
+
+            return (a >= 0 && b >= 0 && c >= 0);
+        }
+
+        private void openGLControl1_Resized(object sender, EventArgs e)
+        {
+            gl = openGLControl1.OpenGL;
+            Control control = (Control)sender;
+            gl.Viewport(0, 0, control.Width, control.Height);
+            gl.MatrixMode(OpenGL.GL_PROJECTION);
+            gl.LoadIdentity();
+            gl.Ortho2D(0, control.Width, control.Height, 0);
+            gl.MatrixMode(OpenGL.GL_MODELVIEW);
+            gl.LoadIdentity();
+        }
+        private void openGLControl1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Z && e.Modifiers == Keys.Control)
+            {
+                if (tris.Count > 0)
+                {
+                    tris.RemoveAt(tris.Count - 1);
+                }
+            }
+        }
+
+        private void openGLControl1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (selectedTriangle != -1 && e.Button == MouseButtons.Left && mode == Mode.Drag)
+            {
+                Point delta;
+                delta.x = (short)(e.X - prevMouseLocation.x);
+                delta.y = (short)(e.Y - prevMouseLocation.y);
+                Triangle curTri = tris[selectedTriangle];
+                curTri.point1 += delta;
+                curTri.point2 += delta;
+                curTri.point3 += delta;
+                tris[selectedTriangle] = curTri;
+
+                prevMouseLocation.x = (short)e.X;
+                prevMouseLocation.y = (short)e.Y;
+            }
+        }
+
+        private void drawButton_Click(object sender, EventArgs e)
+        {
+            mode = Mode.Draw;
+        }
+
+        private void dragButton_Click(object sender, EventArgs e)
+        {
+            mode = Mode.Drag;
+        }
+
+        private void openGLControl1_MouseUp(object sender, MouseEventArgs e)
+        {
+            selectedTriangle = -1;
+        }
+    }
+}
